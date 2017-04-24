fb373f4c28893d4dc7cadace1d5d660bf830c2a6
hina <hina@hinaloe.net>
2017-04-24 23:53:13 +0900

    ボケてた

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         <p>github: <a href="https://github.com/hinaloe/public-toot-viewer"><span class="hidden-xs-down">hinaloe/</span>public-toot-viewer</a> by <a
                 href="https://github.com/hinaloe/">hinaloe</a> on <a href="https://mdn.hinaloe.net">ひな丼</a></p>
       <div class="alert alert-warning" v-if="error">{{error}}</div>
-        <form action="post" @submit.prevent="submit">
+        <form action="/" method="post" @submit.prevent="submit">
             <div class="form-group row">
                 <label for="domain" class="col-4 col-md-2">Domain</label>
                 <input type="text" class="form-control col-8 col-md-4"
