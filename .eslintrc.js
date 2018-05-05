fc9cd09865191808e91fc40765ed5b5e5f14ff06
hina <hina@hinaloe.net>
2018-05-06 03:27:14 +0900

    eslint-plugin-vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,21 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
   env: {
     browser: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: [
+    'plugin:vue/essential',
+    'standard',
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue'
   ],
   // add your custom rules here
   'rules': {
