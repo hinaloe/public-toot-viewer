397c28358efce04b74d306ebed00ce1686fbb995
hina <hina@hinaloe.net>
2022-02-06 01:01:01 +0900

    remove legacy webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,17 @@
 
 module.exports = {
   root: true,
-  parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module',
-  },
+  // parserOptions: {
+  //   parser: 'babel-eslint',
+  //   sourceType: 'module',
+  // },
   env: {
     browser: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: [
     'plugin:vue/recommended',
-    'standard',
+    // 'standard',
   ],
   // required to lint *.vue files
   plugins: [
